31df480ac913322e1ea7759b0b8b4827fac9fb1c
Evyatar Ashuri <46943179+Evyatarashuri@users.noreply.github.com>
2024-08-13 12:57:27 +0300

    Update settings.py

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w17hibtna-&_&j-1dc!3cz9qsbpk*3jr4fkciml6(k9d)2h^x4'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
